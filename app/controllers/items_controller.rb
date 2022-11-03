7e326a77886d10284096040d13f42ac13789248d
Joseph <joeyparlamenti@yahoo.com>
2022-11-02 23:53:14 -0400

    Enabled CRUD routing for main models

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,29 +14,29 @@ class ItemsController < ApplicationController
   end
 
   # POST /items
-  def create
-    @item = Item.new(item_params)
+  # def create
+  #   @item = Item.new(item_params)
 
-    if @item.save
-      render json: @item, status: :created, location: @item
-    else
-      render json: @item.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @item.save
+  #     render json: @item, status: :created, location: @item
+  #   else
+  #     render json: @item.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /items/1
-  def update
-    if @item.update(item_params)
-      render json: @item
-    else
-      render json: @item.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     render json: @item
+  #   else
+  #     render json: @item.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /items/1
-  def destroy
-    @item.destroy
-  end
+  # def destroy
+  #   @item.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :list_items
-  resources :lists
-  resources :users
-  resources :items
+  resources :list_items, except: [:new, :edit]
+  resources :lists, except: [:new, :edit]
+
+  resources :users, only: [:index, :show]
+  resources :items, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
